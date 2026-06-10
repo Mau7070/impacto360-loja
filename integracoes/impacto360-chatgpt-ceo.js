e9cdd83710199e170f05ba2bee50b21f2654b0c0
Mau7070 <108143348+Mau7070@users.noreply.github.com>
2026-06-10 10:36:21 -0300

    Corrigir arquivo ChatGPT CEO Impacto360

--- integracoes/impacto360-chatgpt-ceo.js
+++ integracoes/impacto360-chatgpt-ceo.js
@@ -1,21 +1,31 @@
 (function () {
   "use strict";
 
-  const CEO_NAME = "ChatGPT Chief Executive Officer";
+  const CEO_NAME = "ChatGPT";
+  const CEO_TITLE = "ChatGPT Chief Executive Officer";
   const STORE_NAME = "Shopping Impacto360";
   const MANUAL_PRODUCTS_KEY = "ai360:manualProducts";
   const POSTS_KEY = "ai360:postagens";
 
-  start();
+  startCeo();
 
-  function start() {
+  function startCeo() {
     replaceMonicaText();
     injectStyle();
     createPanel();
+    exposeApi();
+  }
+
+  function exposeApi() {
     window.IMPACTO360_CEO = {
-      nome: CEO_NAME,
+      nome: CEO_TITLE,
       funcao: "Chief Executive Officer do Shopping Impacto360",
       listarProdutos: listProducts,
       salvarProduto: saveProduct,
       arquivarProduto: archiveProduct,
-      moverParaLixeira
+      moverParaLixeira: moveToTrash,
+      gerarPostagem: createPost
+    };
+  }
+
+  function replaceMonicaText
