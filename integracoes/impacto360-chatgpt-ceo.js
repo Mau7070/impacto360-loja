a5263a12acc9445097cf06a05e9ce1110525c1e2
Mau7070 <108143348+Mau7070@users.noreply.github.com>
2026-06-10 10:39:49 -0300

    Completar ChatGPT CEO Impacto360

--- integracoes/impacto360-chatgpt-ceo.js
+++ integracoes/impacto360-chatgpt-ceo.js
@@ -1,31 +1,26 @@
 (function () {
   "use strict";
 
-  const CEO_NAME = "ChatGPT";
   const CEO_TITLE = "ChatGPT Chief Executive Officer";
   const STORE_NAME = "Shopping Impacto360";
   const MANUAL_PRODUCTS_KEY = "ai360:manualProducts";
   const POSTS_KEY = "ai360:postagens";
 
-  startCeo();
+  start();
 
-  function startCeo() {
-    replaceMonicaText();
+  function start() {
+    replaceMonica();
     injectStyle();
     createPanel();
-    exposeApi();
-  }
-
-  function exposeApi() {
     window.IMPACTO360_CEO = {
       nome: CEO_TITLE,
       funcao: "Chief Executive Officer do Shopping Impacto360",
-      listarProdutos: listProducts,
-      salvarProduto: saveProduct,
-      arquivarProduto: archiveProduct,
-      moverParaLixeira: moveToTrash,
-      gerarPostagem: createPost
+      listarProdutos,
+      salvarProduto,
+      arquivarProduto,
+      moverProdutoParaLixeira,
+      gerarPostagem
     };
   }
 
-  function replaceMonicaText
+  function replaceMonica() {
